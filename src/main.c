2d53c6b78e867974417c41136952f99e74fbb38d
Aruchan Thamilchelvan <athamilc@f2r10s8.clusters.42paris.fr>
2025-10-02 14:59:08 +0200

    first

--- src/main.c
+++ src/main.c
@@ -6,25 +6,25 @@
 /*   By: athamilc <athamilc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 11:20:15 by athamilc          #+#    #+#             */
-/*   Updated: 2025/10/02 14:19:48 by athamilc         ###   ########.fr       */
+/*   Updated: 2025/10/02 14:43:51 by athamilc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int main(int argc, char **argv)
-{
-    t_data config;
-// ne pas oublier fonction (.cub)
-    ft_memset(&config, 0, sizeof(config));
-    if (argc != 2)
-    {
-        printf("Invalid number of arguments.\n");
-        return (1);
-    }
-    parser_mlx(&config);
-    init_mlx(&config);
-    draw_mlx(&config);
-    free_mlx(&config);
-    return (0);
-}
+// int main(int argc, char **argv)
+// {
+//     t_data config;
+// // ne pas oublier fonction (.cub)
+//     ft_memset(&config, 0, sizeof(config));
+//     if (argc != 2)
+//     {
+//         printf("Invalid number of arguments.\n");
+//         return (1);
+//     }
+//     parser_mlx(&config);
+//     init_mlx(&config);
+//     draw_mlx(&config);
+//     free_mlx(&config);
+//     return (0);
+// }
