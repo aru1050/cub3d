2162911a6af99ab25581a055d0b5e8543968b9f7
Aruchan THAMILCHELVAN <aruchantha@gmail.com>
2025-11-01 18:34:25 +0100

    leaks_ok

--- src/utils/keys.c
+++ src/utils/keys.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 12:04:24 by athamilc          #+#    #+#             */
-/*   Updated: 2025/11/01 18:05:28 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/01 18:29:52 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,12 +66,46 @@ int	game_loop(t_data *data)
 	return (0);
 }
 
+static void	free_map(t_map *map)
+{
+	int	y;
+
+	if (!map || !map->grid)
+		return ;
+	y = 0;
+	while (y < map->height)
+	{
+		free(map->grid[y]);
+		y++;
+	}
+	free(map->grid);
+	map->grid = NULL;
+}
+
+static void	free_texture_paths(t_data *d)
+{
+	if (d->north.path)
+		free(d->north.path);
+	if (d->south.path)
+		free(d->south.path);
+	if (d->west.path)
+		free(d->west.path);
+	if (d->east.path)
+		free(d->east.path);
+	d->north.path = NULL;
+	d->south.path = NULL;
+	d->west.path = NULL;
+	d->east.path = NULL;
+}
+
 int	close_window(t_data *data)
 {
 	if (!data || data->is_closed)
 		return (0);
 	data->is_closed = 1;
 	destroy_all_textures(data);
+	free_texture_paths(data);
+	free_map(&data->map);
 	if (data->img)
 		mlx_destroy_image(data->mlx, data->img);
 	if (data->win)
